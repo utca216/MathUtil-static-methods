26a3f332589b10b0994e5864945c0b66fb96f17a
utca216 <151619685+utca216@users.noreply.github.com>
2024-09-26 14:57:38 +0600

    Add files via upload

---
+++ MathUntil.java
@@ -0,0 +1,16 @@
+public class MathUntil {
+    public static boolean isPrime(int n){
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static int gcd(int a, int b){
+        
+    }
+}
